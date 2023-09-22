22aef04cfd065c6aa1e1e06518621ec9b1550e57
Sudarshan <123970843+NagulapalliSudarshan@users.noreply.github.com>
2023-09-22 12:13:16 +0530

    Add files via upload

---
+++ GreedyknapSack.py
@@ -0,0 +1,30 @@
+def GreedyKnapSack(p,w,m,n):
+    index = list(range(n))
+    PWratio = [p[i]/w[i] for i in index]
+    print(PWratio)
+    index.sort(key=lambda i:PWratio[i],reverse=True)
+    print(PWratio)
+    maxVal = 0
+    x=[0]*n
+    u=m
+    for i in range(n):
+        if w[i]>u : 
+            break
+        x[i] = 1
+        maxVal += p[i]*x[i]
+        u=u-w[i]
+    if i<=n:
+        x[i] = u/w[i]
+    maxVal += p[i]*x[i]
+    
+    return maxVal,x
+
+m= int(input("Enter m :"))
+
+# print("Enter the profits : ",end=" ")
+profits = [int(x) for x in input().split()]
+
+# print("Enter the weights : ",end=" ")
+weights = [int(x) for x in input().split()]
+
+print(GreedyKnapSack(profits,weights,m,len(profits)))
