7462ae6f7787dd14ddd46a0f9c2559ce57704d9f
sudarshan <sudarshan.nagulapalli@gmail.com>
2023-10-03 23:49:58 +0530

    KnapSack Solved Issue

--- GreedyknapSack.py
+++ GreedyknapSack.py
@@ -1,31 +1,34 @@
 def GreedyKnapSack(p,w,m,n):
     index = list(range(n))
     PWratio = [p[i]/w[i] for i in index]
-    print(PWratio)
+    # print(PWratio)
     index.sort(key=lambda i:PWratio[i],reverse=True)
-    print(PWratio)
+    # print(index)
+    # print(PWratio)
     maxVal = 0
     x=[0]*n
     u=m
-    for i in range(n):
+    for i in index:
         if w[i]>u : 
             break
         x[i] = 1
         maxVal += p[i]*x[i]
         u=u-w[i]
+        print(x,maxVal)
     if i<=n:
         x[i] = u/w[i]
     maxVal += p[i]*x[i]
+    print(x,maxVal)
     
     return maxVal,x
 
 # weight of the knapsack 
-m= int(input("Enter m :"))
+m= int(input("Enter m (knapsack max weight) :"))
 
-# print("Enter the profits : ",end=" ")
+print("Enter the profits : ",end=" ")
 profits = [int(x) for x in input().split()]
 
-# print("Enter the weights : ",end=" ")
+print("Enter the weights : ",end=" ")
 weights = [int(x) for x in input().split()]
 
 print(GreedyKnapSack(profits,weights,m,len(profits)))
